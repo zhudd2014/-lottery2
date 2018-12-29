21ef6a9fec01641d73a9576838ce225d0a5148cb
lookzhang <89255371@qq.com>
2018-12-29 12:30:11 +0800

    删除无用代码

--- miniprogram/pages/eventSuc/eventSuc.js
+++ miniprogram/pages/eventSuc/eventSuc.js
@@ -5,80 +5,107 @@ Page({
    * 页面的初始数据
    */
   data: {
-    event_suc:''
+    event_suc: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    const db = wx.cloud.database()
-    // 查询当前用户所有的 counters
-    db.collection('event_success').where({
-      event_id: 'XCYin4nnuWjciuy7'
-    }).get({
+  onLoad: function(options) {
+    wx.cloud.callFunction({
+      name: 'getEventSuc',
+      data: {
+        event_id: 'XCYin4nnuWjciuy7',
+      },
       success: res => {
-        this.setData({
-          event_suc: JSON.stringify(res.data, null, 2),
+        wx.showToast({
+          title: '调用成功',
         })
-        console.log('[数据库event_success] [查询记录] 成功: ', res.data)
+        console.log('[云函数getEventSuc调用] 成功: ', res.data)
+     
       },
       fail: err => {
         wx.showToast({
           icon: 'none',
-          title: '查询记录失败'
+          title: '调用失败',
         })
-        console.error('[数据库] [查询记录] 失败：', err)
+        console.error('[云函数] [sum] 调用失败：', err)
       }
     })
   },
 
+
+  // /**
+  //  * 生命周期函数--监听页面加载
+  //  */
+  // onLoad: function(options) {
+  //   const db = wx.cloud.database()
+  //   // 查询当前用户所有的 counters
+  //   db.collection('event_success').where({
+  //     event_id: 'XCYin4nnuWjciuy7'
+  //   }).get({
+  //     success: res => {
+  //       this.setData({
+  //         event_suc: JSON.stringify(res.data, null, 2),
+  //       })
+  //       console.log('[数据库event_success] [查询记录] 成功: ', res.data)
+  //     },
+  //     fail: err => {
+  //       wx.showToast({
+  //         icon: 'none',
+  //         title: '查询记录失败'
+  //       })
+  //       console.error('[数据库] [查询记录] 失败：', err)
+  //     }
+  //   })
+  // },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
