67a3d186bb3c878219c09adc76276cede664b570
xuyonghua <1600062751@qq.com>
2018-12-29 23:04:22 +0800

    openid

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -19,6 +19,7 @@ Page({
       })
       return
     }
+    this.onGetOpenid()
 
     // 获取用户信息
     wx.getSetting({
@@ -60,10 +61,7 @@ Page({
       data: {},
       success: res => {
         console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
+        wx.setStorageSync('openid', res.result.openid)
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
