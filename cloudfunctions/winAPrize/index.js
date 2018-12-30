f4b5fa473ae33d1ee4353d265698b9ece8669810
lookzhang <89255371@qq.com>
2018-12-30 14:07:03 +0800

    添加云函数

---
+++ cloudfunctions/winAPrize/index.js
@@ -0,0 +1,15 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+
+  console.log('event' + event)
+  console.log(event.userInfo.openId)
+  
+  return {
+    winAPrize: true
+  }
+}
